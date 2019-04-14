41e28739a053b793645a044e97fdbae809ade1f9
kciam001 <kciam001@ucr.edu>
2019-04-14 13:18:59 -0700

    Added animation scripting

---
+++ Assets/character_animator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_animator : MonoBehaviour
+{
+    Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalkingPressed;
+        if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right"))
+            isWalkingPressed = true;
+        else
+            isWalkingPressed = false;
+
+        myAnimator.SetBool("IsWalking", isWalkingPressed);
+        Debug.Log("Test: " + isWalkingPressed);
+    }
+}
