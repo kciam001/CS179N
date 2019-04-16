16223789ab65211ccf3e7951da3939ae43743894
Sidney Son <psndnb@gmail.com>
2019-04-16 08:43:12 -0700

    Added scripts folder

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public Transform target, player;
+    float mouseX, mouseY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CameraControl();
+    }
+
+    void CameraControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+            target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+}
