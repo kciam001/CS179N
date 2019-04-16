13d4f96776605ba6e631b3cdaf739a97c56f8ea9
qijieguan <qijieguan7@gmail.com>
2019-04-16 08:45:12 -0700

    Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerController : MonoBehaviour {
+    public LayerMask movementMask;  
+    Camera cam;
+    PlayerMovement move;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        move = GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                move.MoveToPoint(hit.point);
+            }
+        }
+	}
+}
