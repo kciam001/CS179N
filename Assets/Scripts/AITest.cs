e0e37576a589d59b34c3aa7ef80eb41af2b0ac1b
Sidney Son <psndnb@gmail.com>
2019-04-16 10:01:10 -0700

    Added test AI

---
+++ Assets/Scripts/AITest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITest : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .03f);
+    }
+}
