9c25cf215cf495c21b69f173efbd5d8d30690fa8
kciam001 <kciam001@ucr.edu>
2019-04-23 09:36:32 -0700

    Added hero attack animation

--- Assets/Scripts/character_animator.cs
+++ Assets/Scripts/character_animator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class character_animator : MonoBehaviour
 {
     Animator myAnimator;
+    float elapsedTime;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +15,31 @@ public class character_animator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool isWalkingPressed;
-        if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")
-           || Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
-            isWalkingPressed = true;
-        else
-            isWalkingPressed = false;
+        bool isWalkingPressed = CheckWalking();
+        bool isAttackPressed = CheckAttack(); 
+
 
         myAnimator.SetBool("IsWalking", isWalkingPressed);
-        // Debug.Log("Test: " + isWalkingPressed);
+        myAnimator.SetBool("IsAttacking", isAttackPressed);
+        Debug.Log("Test: " + isAttackPressed);
+
+    }
+
+    bool CheckWalking()
+    {
+        if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")
+           || Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+            return true;
+        else
+            return false;
+
+
+    }
+    bool CheckAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+            return true;
+        else
+            return false;
     }
 }
