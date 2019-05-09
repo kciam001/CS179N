0d7eb79d5cc015e4c541dd3952d127bcb98e9b1f
Tommy Shiels <tommyshiels@gmail.com>
2019-05-08 20:39:17 -0700

    Fixed character movement and sprint
    Make player face direction of movement

--- Assets/Scripts/ThirdPersonCameraController.cs
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ThirdPersonCameraController : MonoBehaviour
 {
+    /*
     public float CameraMoveSpeed = 120.0f;
     public Transform target;
     public Transform player;
@@ -62,5 +63,86 @@ public class ThirdPersonCameraController : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         player.rotation = Quaternion.Euler(0, rotY, 0);
 
+    }
+    */
+    [Header("Drag the Object you want to follow here")]
+    [Space(5)]
+    [Tooltip("The best way to use this is to create an empty gameobject and parent it to your players hip/pelvis bone.")]
+    public GameObject CameraFollowObj;
+
+    [Header("Adjust these values to how you want the camera to rotate")]
+    [Space(5)]
+    public float CameraMoveSpeed = 120.0f;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+
+    Vector3 FollowPOS;
+    GameObject CameraObj;
+    GameObject PlayerObj;
+    float camDistanceXToPlayer;
+    float camDistanceYToPlayer;
+    float camDistanceZToPlayer;
+    float mouseX;
+    float mouseY;
+    float finalInputX;
+    float finalInputZ;
+    float smoothX;
+    float smoothY;
+    float rotY = 0.0f;
+    float rotX = 0.0f;
+    float inputX;
+    float inputZ;
+    GameObject player;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // We setup the rotation of the sticks here
+        inputX = Input.GetAxis("RightStickHorizontal");
+        inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+
+
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        // set the target object to follow
+        Transform target = CameraFollowObj.transform;
+
+        //move towards the game object that is the target
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 }
--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -7,7 +7,7 @@ public class character : MonoBehaviour
 {
     public float speed = 1;
     int sprint_factor = 3;
-    int stamina_factor = 10;
+    int stamina_factor = 5;
     float stamina = 0;
     // float timer = 0;
     float max_stamina = 10;
@@ -15,14 +15,23 @@ public class character : MonoBehaviour
     Rect stamina_rect;
     Texture2D stamina_texture;
     Texture2D tired_texture;
-    Vector3 playerMovement;
+    Vector3 playerMovement = Vector3.zero;
 
     public Text stamina_text;
     public Image stamina_bar;
     private Color red = Color.red;
     private Color green = Color.green;
+
+    float velocity = 5f;
+    float turnSpeed = 10;
+    Vector2 input;
+    float angle;
+    Quaternion targetRotation;
+    Transform cam;
+
     void Start()
     {
+        cam = Camera.main.transform;
         red.a = 0.3f;
         green.a = 0.3f;
     }
@@ -45,29 +54,65 @@ public class character : MonoBehaviour
             stamina_bar.fillAmount = 0;
             stamina_text.text = "";
         }
+        GetInput();
+
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1)
+        {
+            Move();
+            return;
+        }
+        CalculateDirection();
+        Rotate();
         Move();
+    }
+
+    void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
     }
 
     void Move()
     {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.forward * velocity * Time.deltaTime;
+        }
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
 
         //sprint
         if (Input.GetKey(KeyCode.LeftShift) && stamina < max_stamina && !stamina_reset)
         {
 
-            playerMovement = new Vector3(x, 0f, y) * speed * sprint_factor * Time.deltaTime;
+            //playerMovement = new Vector3(x, 0f, y) * speed * sprint_factor * Time.deltaTime;
+            transform.position += transform.forward * velocity * sprint_factor * Time.deltaTime;
             stamina += stamina_factor * Time.deltaTime;
         }
         else if (stamina < max_stamina && stamina > 0 && !stamina_reset)
         {
-            playerMovement = new Vector3(x, 0f, y) * speed * Time.deltaTime;
+            //playerMovement = new Vector3(x, 0f, y) * speed * Time.deltaTime;
+            //transform.position += transform.forward * velocity * Time.deltaTime;
             stamina -= (stamina_factor * 0.25f) * Time.deltaTime;
         }
         else
         {
-            playerMovement = new Vector3(x, 0f, y) * speed * Time.deltaTime;
+            //playerMovement = new Vector3(x, 0f, y) * speed * Time.deltaTime;
+            //transform.position += transform.forward * velocity * Time.deltaTime;
             if (stamina >= max_stamina && !stamina_reset)
             {
                 stamina_reset = true;
@@ -78,7 +123,7 @@ public class character : MonoBehaviour
                 stamina_reset = false;
 
         }
-        transform.Translate(playerMovement, Space.Self);
-
+        
+        //transform.Translate(playerMovement, Space.Self);
     }
 }
