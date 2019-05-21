2f30e2c46bbf6385a2526c4669f2706f2cacd8e3
kciam001 <kciam001@ucr.edu>
2019-05-21 10:43:20 -0700

    Added Pause Screen
    -'P' to pause

--- Assets/Scripts/AITest.cs
+++ Assets/Scripts/AITest.cs
@@ -9,7 +9,9 @@ public class AITest : MonoBehaviour
     public Animator anim;
     bool isHurt = false;
     bool playerDead = false;
+    public bool isPaused;
     string enemyName;
+    public PauseMenu Pause;
    
     // Start is called before the first frame update
     void Start()
@@ -26,57 +28,61 @@ public class AITest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerDead = player.GetComponent<PlayerHealth>().cur_health <= 0;
-        float dist = Vector3.Distance(player.transform.position, this.transform.position);
+        isPaused = Pause.isPaused;
+        if (!isPaused)
+        {
+            playerDead = player.GetComponent<PlayerHealth>().cur_health <= 0;
+            float dist = Vector3.Distance(player.transform.position, this.transform.position);
 
-        if (playerDead)
-        {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isRunning", false);
-            anim.SetBool("isAttacking", false);
-        }
-        else if(this.GetComponent<EnemyHealth>().cur_health >= 0)
-        {
-            if (dist < 100) //awake range
+            if (playerDead)
             {
-                transform.LookAt(player.transform.position);
-
-                transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * 0.01f);
-
-                anim.SetBool("isIdle", false);
-                if (enemyName.Contains("Lich"))
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isAttacking", false);
+            }
+            else if (this.GetComponent<EnemyHealth>().cur_health >= 0)
+            {
+                if (dist < 100) //awake range
                 {
-                    if (dist > 20)
+                    transform.LookAt(player.transform.position);
+
+                    transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * 0.01f);
+
+                    anim.SetBool("isIdle", false);
+                    if (enemyName.Contains("Lich"))
+                    {
+                        if (dist > 20)
+                        {
+                            anim.SetBool("isRunning", true);
+                            anim.SetBool("isAttacking", false);
+
+                        }
+                        else
+                        {
+
+                            anim.SetBool("isAttacking", true);
+                            anim.SetBool("isRunning", false);
+                        }
+
+                    }
+                    else if (dist > 5)
                     {
                         anim.SetBool("isRunning", true);
                         anim.SetBool("isAttacking", false);
-
                     }
                     else
                     {
-
                         anim.SetBool("isAttacking", true);
                         anim.SetBool("isRunning", false);
                     }
 
                 }
-                else if (dist > 5)
+                else //idle range
                 {
-                    anim.SetBool("isRunning", true);
-                    anim.SetBool("isAttacking", false);
-                }
-                else
-                {
-                    anim.SetBool("isAttacking", true);
+                    anim.SetBool("isIdle", true);
                     anim.SetBool("isRunning", false);
+                    //anim.SetBool("isAttacking", false);
                 }
-
-            }
-            else //idle range
-            {
-                anim.SetBool("isIdle", true);
-                anim.SetBool("isRunning", false);
-                //anim.SetBool("isAttacking", false);
             }
         }
     }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused;
+    public Canvas pauseMenu;
+    public Canvas hudMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+    }
+
+    void getInput()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(Time.timeScale == 1)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+    void PauseGame()
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        pauseMenu.enabled = true;
+        hudMenu.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void ResumeGame()
+    {
+        isPaused = false;
+        Time.timeScale = 1;
+        pauseMenu.enabled = false;
+        hudMenu.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
